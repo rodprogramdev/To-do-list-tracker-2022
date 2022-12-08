86a07118fb802e8f892c89f6c6c00a2773024a50
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-12-08 16:27:48 +0800

    Updates

---
+++ precache-manifest.b822b0a040fc83b8214609cbb7e9786c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b40e06729ff0971b80a1",
+    "url": "/To-do-list-tracker-2022/static/css/main.3104ea55.chunk.css"
+  },
+  {
+    "revision": "b40e06729ff0971b80a1",
+    "url": "/To-do-list-tracker-2022/static/js/main.b40e0672.chunk.js"
+  },
+  {
+    "revision": "69e0dab03ec9f6c99eb0",
+    "url": "/To-do-list-tracker-2022/static/js/1.69e0dab0.chunk.js"
+  },
+  {
+    "revision": "44faef076067e302bbdd",
+    "url": "/To-do-list-tracker-2022/static/js/2.44faef07.chunk.js"
+  },
+  {
+    "revision": "d6d41e1740b2557b5c91",
+    "url": "/To-do-list-tracker-2022/static/js/runtime~main.d6d41e17.js"
+  },
+  {
+    "revision": "57f4e83f6f0cab1618b75dbb51937d0d",
+    "url": "/To-do-list-tracker-2022/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/To-do-list-tracker-2022/precache-manifest.b822b0a040fc83b8214609cbb7e9786c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/To-do-list-tracker-2022/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
